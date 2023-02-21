7d1f69beb19bff44d57140c7fd57b95ab89a29a6
lucie-sio <81620468+lucie-sio@users.noreply.github.com>
2023-02-21 20:23:23 +0100

    gestions icônes couleurs

--- src/navigation/TabBar.jsx
+++ src/navigation/TabBar.jsx
@@ -5,6 +5,7 @@ import { Accueil } from "../pages/Accueil";
 import { Images } from "../pages/Images";
 import { About } from "../pages/About";
 // Icônes
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import HomeIcon from "../../assets/home.svg";
 import CatIcon from "../../assets/cat.svg";
 import InfoIcon from "../../assets/info.svg";
@@ -12,54 +13,53 @@ import InfoIcon from "../../assets/info.svg";
 const Tab = createBottomTabNavigator();
 
 export function TabNavigator() {
-    return (
-        <Tab.Navigator
-            screenOptions={{
-                tabBarActiveTintColor: "#fe9695",
-                tabBarInactiveTintColor: "black",
-                headerShown: false,
-            }}
-        >
-            <Tab.Screen
-                options={{
-                    tabBarIcon: () => (
-                        <Image
-                            style={{ width: 24, height: 24 }}
-                            source={{
-                                uri: "https://cdn-icons-png.flaticon.com/512/1946/1946488.png",
-                            }}
-                        />
-                    ),
-                }}
-                name="Accueil"
-                component={Accueil}
-            />
-            <Tab.Screen
-                options={{
-                    tabBarIcon: () => (
-                        <Image
-                            style={{ width: 24, height: 24 }}
-                            source={{
-                                uri: "https://cdn-icons-png.flaticon.com/512/739/739249.png",
-                            }}
-                        />
-                    ),
-                }}
-                name="Images"
-                component={Images}
-            />
-            <Tab.Screen
-                options={{
-                    tabBarIcon: () => (
-                        <Image
-                            style={{ width: 24, height: 24, color: "red" }}
-                            source={InfoIcon}
-                        />
-                    ),
-                }}
-                name="About"
-                component={About}
-            />
-        </Tab.Navigator>
-    );
+	return (
+		<Tab.Navigator
+			screenOptions={{
+				tabBarActiveTintColor: "#fe9695",
+				tabBarInactiveTintColor: "black",
+				headerShown: false,
+			}}
+		>
+			<Tab.Screen
+				options={{
+					tabBarIcon: ({ focused }) => (
+						<Icon
+							name="home-outline"
+							color={focused ? "#fe9695" : "black"}
+							size={30}
+						/>
+					),
+				}}
+				name="Accueil"
+				component={Accueil}
+			/>
+			<Tab.Screen
+				options={{
+					tabBarIcon: ({ focused }) => (
+						<Icon
+							name="cat"
+							color={focused ? "#fe9695" : "black"}
+							size={30}
+						/>
+					),
+				}}
+				name="Images"
+				component={Images}
+			/>
+			<Tab.Screen
+				options={{
+					tabBarIcon: ({ focused }) => (
+						<Icon
+							name="information-outline"
+							color={focused ? "#fe9695" : "black"}
+							size={30}
+						/>
+					),
+				}}
+				name="About"
+				component={About}
+			/>
+		</Tab.Navigator>
+	);
 }
